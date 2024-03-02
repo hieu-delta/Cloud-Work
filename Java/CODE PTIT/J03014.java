8b9653c15fe100e3bb1cc64a06c10d23d40384db
Hieu Hom Hinh <143479576+hieu-delta@users.noreply.github.com>
2024-03-02 19:19:26 +0700

    Add files via upload

---
+++ Java/CODE PTIT/J03014.java
@@ -0,0 +1,13 @@
+// import java.util.*;
+// import java.math.*;
+// public class J03014 {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int t = 1;
+//         while(t-->0){
+//             BigInteger a = new BigInteger(sc.next());
+//             BigInteger b = new BigInteger(sc.next());
+//             System.out.println(a.add(b));
+//         }
+//     }
+// }
